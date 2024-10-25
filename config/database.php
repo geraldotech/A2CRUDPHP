c82d0ba0ee37cc7228f367104db44d5ea5d55214
Geraldo Costa Filho <92253544+geraldotech@users.noreply.github.com>
2024-10-25 16:09:53 -0300

    ADD DATABASE

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 // Database configuration
-$DB_HOST = 'localhost';
-$DB_NAME = 'revendafilial';
-$DB_USER = 'root';
-$DB_PASS = '';
+$DB_HOST = 'sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$DB_NAME = 'qbbzzx6kg3koxx7y';
+$DB_USER = 'sttyw86jbah9ghmk';
+$DB_PASS = 'adi9h23cf399fk6s';
 
 try {
     $conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
